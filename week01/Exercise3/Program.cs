fad4bf6a4f01a7c75086c455314f5f67f3c0881e
ramirotl <tapiara22@gmail.com>
2025-05-11 23:40:24 -0400

    Finished Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.Write("You guessed it!");
+            }
+        }
+
     }
 }
